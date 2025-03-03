dd10f58066a184bf82614f42e324a86be3fd2669
Monametsi-s <elboncaptain@gmail.com>
2025-03-03 21:34:55 +0200

    Added Movie Card feature

--- frontend-movie-web/src/App.jsx
+++ frontend-movie-web/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div >
+      <Text text="Monametsi"/>
+    </div>
+    //JSX yntax
+    //A component - Any function in Javascript that returns some 
+    //kind of JSX code.
+    //They start with a capital letter. Here App is a component
+    //Jsx components returns only one parent element
+    //fragments are empty html elements that allows us to return multiple parent elements
   )
 }
 
+function Text({text}) {
+  return (
+    <div>
+      <p>Hello {text}</p>
+    </div>
+  )
+}
 export default App
