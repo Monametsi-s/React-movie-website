c04d12f40201629a2659637a6dd35a8832e7ae63
Monametsi-s <elboncaptain@gmail.com>
2025-03-09 12:17:04 +0200

    Added functionaly to the favourites feature

--- frontend-movie-web/src/pages/Favorites.jsx
+++ frontend-movie-web/src/pages/Favorites.jsx
@@ -1,11 +1,29 @@
-import '../css/Favorites.css'
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
-    return <div className="favorites-empty">
-        <h2>No favorite movies</h2>
-        <p>Start adding movies to your favourites and they will appear here</p>
-    </div>
-    
+  const { favorites } = useMovieContext();
+
+ 
+    return (
+      <div className="favorites">
+        <h2>My favorite Movies</h2>
+        {favorites.length > 0 ? (
+            <div className="movies-grid">
+            {favorites.map(
+              (movie) => (
+                <MovieCard movie={movie} key={movie.id} />
+                ))}
+          </div>
+        ) : (
+            <div className="favorites-empty">
+                <h2>No favorite movies</h2>
+                <p>Start adding movies to your favourites and they will appear here</p>
+          </div>
+          )}
+      </div>
+    );
 }
 
-export default Favorites
+export default Favorites;
